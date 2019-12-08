818990621efc10cc173ec87ce36e659aa2acf3ce
akorb <andreas.d.korb@gmail.com>
2019-12-08 21:38:40 +0900

    updated benchmark and removed generated file

--- benchmark.sh
+++ benchmark.sh
@@ -8,18 +8,18 @@ do_benchmark() {
     for nprocs in "1" "2" "4" "8" "16" "32" "64" "128";
     do ./glife sample_inputs/$1 $nprocs 10000 $2 $3 >> benchmark.m; done
     echo '];' >> benchmark.m
-    echo "plot(x, $1_$2x$3, 'r-*', 'DisplayName', '$1_$2x$3')" >> benchmark.m
+    echo "plot(x, $1_$2x$3, '$4-o', 'DisplayName', '$1_$2x$3', 'LineWidth', 1.35, 'MarkerFaceColor', '$4')" >> benchmark.m
     echo 'hold on' >> benchmark.m
 }
 
-do_benchmark 'make' 1000 1000
-do_benchmark 'make' 100 100
+do_benchmark 'make' 1000 1000 'r'
+do_benchmark 'make' 100 100 'b'
 
-do_benchmark 'puf' 1000 1000
-do_benchmark 'puf' 100 100
+do_benchmark 'puf' 1000 1000 'k'
+do_benchmark 'puf' 300 300 'g'
 
-do_benchmark 'glider' 10000 200
-do_benchmark 'glider' 200 10000
+do_benchmark 'glider' 10000 200 'm'
+do_benchmark 'glider' 200 10000 'c'
 
 echo 'hold off' >> benchmark.m
 echo "legend('Interpreter', 'none')" >> benchmark.m
