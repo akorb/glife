a6bf0434e3427b88af288022785abf72aaf9c133
akorb <andreas.d.korb@gmail.com>
2019-12-09 16:30:58 +0900

    Added output while benchmark is running

--- benchmark.sh
+++ benchmark.sh
@@ -4,9 +4,12 @@ echo 'close all' > benchmark.m
 echo 'x = [ 1 2 4 8 16 32 64 128 ];' >> benchmark.m
 
 do_benchmark() {
+    
     echo -n "$1_$2x$3 = [ " >> benchmark.m
-    for nprocs in "1" "2" "4" "8" "16" "32" "64" "128";
-    do ./glife sample_inputs/$1 $nprocs 10000 $2 $3 >> benchmark.m; done
+    for nprocs in "1" "2" "4" "8" "16" "32" "64" "128"; do
+        echo "Benchmarking $1 $2x$3 with $nprocs threads"
+        ./glife sample_inputs/$1 $nprocs 10000 $2 $3 >> benchmark.m
+    done
     echo '];' >> benchmark.m
     echo "plot(x, $1_$2x$3, '$4-o', 'DisplayName', '$1_$2x$3', 'LineWidth', 1.35, 'MarkerFaceColor', '$4')" >> benchmark.m
     echo 'hold on' >> benchmark.m
